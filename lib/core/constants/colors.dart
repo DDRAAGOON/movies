ef10568d236b5c60639bdc63a6b9a3969b9ff075
DRAGON <mahmoudessam936@gmail.com>
2025-11-07 17:56:14 +0200

    files

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors();
+
+  static const Color background = Color(0xFF121312);
+  static const Color secondary = Color(0xFF282A28);
+  static const Color primary = Color(0xFFFFBB3B);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color green = Color(0xFF57AA53);
+  static const Color red = Color(0xFFE82626);
+}
